3404486313092a101c7f13c6b4aa08fe3b3a255e
Hasala2002 <hasala.h@hotmail.com>
2023-11-15 22:00:45 -0600

    Fixed text alignment issue

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -5,69 +5,69 @@ import { IconBrandGithub, IconCode, IconCoffee, IconFlame, IconHeartHandshake, I
 
 const Community = () => {
 
-    const handleMouseMove = e =>{
-        const  {  currentTarget: target } = e;
+    const handleMouseMove = e => {
+        const { currentTarget: target } = e;
         const rect = target.getBoundingClientRect(),
-              x = e.clientX - rect.left,
-              y = e.clientY - rect.top
-        
-          target.style.setProperty("--mouse-x", `${x}px`);
-          target.style.setProperty("--mouse-y", `${y}px`);
-      }
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top
 
-  return (
-    <div className={styles.CommunitySection} id="community">
-        <div className={styles.CommunityTitle}>
-            <div className={styles.Divider}></div>
-            <h2>at arkē <img src={Heart} alt="Heart" /> we open-source</h2>
-            <div className={styles.Divider}></div>
+        target.style.setProperty("--mouse-x", `${x}px`);
+        target.style.setProperty("--mouse-y", `${y}px`);
+    }
+
+    return (
+        <div className={styles.CommunitySection} id="community">
+            <div className={styles.CommunityTitle}>
+                <div className={styles.Divider}></div>
+                <h2>at arkē we <img src={Heart} alt="Heart" /> open-source</h2>
+                <div className={styles.Divider}></div>
+            </div>
+            <div className={styles.Community}>
+                <div className={styles.CommunityCard} onMouseMove={handleMouseMove}>
+                    <div className={styles.Title}>
+                        <IconBrandGithub stroke={3} />
+                        <h2>Contribute</h2>
+                    </div>
+                    <p>Keep track of development in github by giving us a star. Open issues and feature requests. You can help us too? You’re more than welcome.</p>
+                    <div className={styles.BtnGroup}>
+                        <a href="https://github.com/Hasala2002/arke/issues" className={styles.BtnBordered}>
+                            <IconCode color={"#72128f"} size={19} />
+                            Issues
+                        </a>
+                        <a href="https://github.com/Hasala2002/arke" className={styles.BtnColored}>
+                            <IconStar color={"#fff"} size={19} />
+                            Star us
+                        </a>
+                    </div>
+                </div>
+                <div className={styles.CommunityCard} onMouseMove={handleMouseMove}>
+                    <div className={styles.Title}>
+                        <IconFlame stroke={3} />
+                        <h2>Connect</h2>
+                    </div>
+                    <p>Want to keep track of updates and new features? Want to find about new release dates and other related news? Follow us now!</p>
+                    <div className={styles.BtnGroup}>
+                        <a href="https://github.com/Hasala2002/arke" className={styles.BtnBordered}>
+                            Follow
+                        </a>
+                    </div>
+                </div>
+                <div className={styles.CommunityCard} onMouseMove={handleMouseMove}>
+                    <div className={styles.Title}>
+                        <IconHeartHandshake stroke={3} />
+                        <h2>Donate</h2>
+                    </div>
+                    <p>We do this for fun, but donations are appreciated. Thanks for the support!</p>
+                    <div className={styles.BtnGroup}>
+                        <a href="https://www.buymeacoffee.com/hhasala200m" className={styles.BtnBordered}>
+                            <IconCoffee color={"#72128f"} size={19} />
+                            Buy me a coffee
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-    <div className={styles.Community}>
-        <div className={styles.CommunityCard} onMouseMove={handleMouseMove}>
-            <div className={styles.Title}>
-                <IconBrandGithub stroke={3} />
-                <h2>Contribute</h2>
-            </div>
-            <p>Keep track of development in github by giving us a star. Open issues and feature requests. You can help us too? You’re more than welcome.</p>
-            <div className={styles.BtnGroup}>
-                <a href="https://github.com/Hasala2002/arke/issues" className={styles.BtnBordered}>
-                    <IconCode color={"#72128f"} size={19} />
-                    Issues
-                </a>
-                <a href="https://github.com/Hasala2002/arke" className={styles.BtnColored}>
-                    <IconStar color={"#fff"} size={19} />
-                    Star us
-                </a>
-            </div>
-        </div>
-        <div className={styles.CommunityCard} onMouseMove={handleMouseMove}>
-                        <div className={styles.Title}>
-                <IconFlame stroke={3} />
-                <h2>Connect</h2>
-            </div>
-            <p>Want to keep track of updates and new features? Want to find about new release dates and other related news? Follow us now!</p>
-            <div className={styles.BtnGroup}>
-                <a href="https://github.com/Hasala2002/arke" className={styles.BtnBordered}>
-                    Follow
-                </a>
-            </div>
-        </div>
-        <div className={styles.CommunityCard} onMouseMove={handleMouseMove}>
-                        <div className={styles.Title}>
-                <IconHeartHandshake stroke={3} />
-                <h2>Donate</h2>
-            </div>
-            <p>We do this for fun, but donations are appreciated. Thanks for the support!</p>
-            <div className={styles.BtnGroup}>
-                <a href="https://www.buymeacoffee.com/hhasala200m" className={styles.BtnBordered}>
-                    <IconCoffee color={"#72128f"} size={19} />
-                    Buy me a coffee
-                </a>
-            </div>
-        </div>
-    </div>
-    </div>
-  )
+    )
 }
 
 export default Community
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -4,34 +4,34 @@ import * as styles from './styles/Highlights.module.scss'
 
 
 const Highlights = () => {
-  return (
-    <div className={styles.Highlights} id="highlights">
-        <div className={styles.Highlight}>
-            <div className={styles.Glow}></div>
-            <IconSpyOff size={45} stroke={2} />
-            <h1>incognito</h1>
-            <p>Chat without worry as nothing is saved. Everthing will be deleted when you close your chat box.</p>
-        </div>
-        <div className={styles.Highlight}>
+    return (
+        <div className={styles.Highlights} id="highlights">
+            <div className={styles.Highlight}>
+                <div className={styles.Glow}></div>
+                <IconSpyOff size={45} stroke={2} />
+                <h1>incognito</h1>
+                <p>Chat without worry as nothing is saved. Everthing will be deleted when you close your chat box.</p>
+            </div>
+            {/* <div className={styles.Highlight}>
             <div className={styles.Glow}></div>
             <h2 style={{height:45}}>e2e</h2>
             <h1>encrypted</h1>
             <p>Any and all messages you send are encrypted end to end. No more unwanted snoopers.</p>
+        </div> */}
+            <div className={styles.Highlight}>
+                <div className={styles.Glow}></div>
+                <IconBrandSocketIo size={45} stroke={2} />
+                <h1>sockets</h1>
+                <p>Directly join your rooms via peer-to-peer connections. No more databases.</p>
+            </div>
+            <div className={styles.Highlight}>
+                <div className={styles.Glow}></div>
+                <IconBrandOpenSource size={45} stroke={2} />
+                <h1>open-source</h1>
+                <p>Arkē is fully open source with an MIT License. Please feel free to contribute.</p>
+            </div>
         </div>
-        <div className={styles.Highlight}>
-            <div className={styles.Glow}></div>
-            <IconBrandSocketIo size={45} stroke={2} />
-            <h1>sockets</h1>
-            <p>Directly join your rooms via peer-to-peer connections. No more databases.</p>
-        </div>
-        <div className={styles.Highlight}>
-            <div className={styles.Glow}></div>
-            <IconBrandOpenSource size={45} stroke={2} />
-            <h1>open-source</h1>
-            <p>Arkē is fully open source with an MIT License. Please feel free to contribute.</p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Highlights
