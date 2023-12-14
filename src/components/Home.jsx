c8d3e98f89ad0381487606ff5c790e800ba146b9
Hasala2002 <hasala.h@hotmail.com>
2023-12-14 17:29:10 -0600

    arke angel animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,100 +1,141 @@
 import { IconChevronRight } from '@tabler/icons'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import * as styles from "./styles/Home.module.scss"
 import { motion } from 'framer-motion'
 import Hero from "../assets/hero.png"
+import Arke from "../assets/arke.png"
 
 const Home = () => {
 
-    const MainButton = useRef()
+  const MainButton = useRef()
 
-    const handleMouseMove = e =>{
-        const  {  currentTarget: target } = e;
-        const rect = target.getBoundingClientRect(),
-              x = e.clientX - rect.left,
-              y = e.clientY - rect.top
-        
-          target.style.setProperty("--mouse-x", `${x}px`);
-          target.style.setProperty("--mouse-y", `${y}px`);
-      }
+  const handleMouseMove = e => {
+    const { currentTarget: target } = e;
+    const rect = target.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top
 
-    useEffect(() => {
-      MainButton.current.onmousemove = e => handleMouseMove(e)
-    }, [])
-    
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+  }
+
+  useEffect(() => {
+    MainButton.current.onmousemove = e => handleMouseMove(e)
+  }, [])
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollVelocity, setScrollVelocity] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+
+      const newScrollPosition = document.getElementById("appWrap").scrollTop;
+
+      setScrollVelocity(newScrollPosition - scrollPosition);
+      setScrollPosition(newScrollPosition);
+
+      console.table({ "Scroll Position": newScrollPosition, "Scroll Velocity": newScrollPosition - scrollPosition })
+    };
+
+    const handleScrollEnd = () => {
+      setScrollVelocity(0);
+    };
+
+    // Attach the scroll event listener when the component mounts
+    document.getElementById("appWrap").addEventListener('scroll', handleScroll);
+    document.getElementById("appWrap").addEventListener("scrollend", handleScrollEnd);
+
+    // Detach the scroll event listener when the component unmounts
+    return () => {
+      document.getElementById("appWrap").removeEventListener('scroll', handleScroll);
+      document.getElementById("appWrap").addEventListener("scrollend", handleScrollEnd);
+    };
+  }, []); // Empty dependency array means this effect will only run once on mount
+
+
+  useEffect(() => {
+    const updateAnimation = () => {
+      setScrollPosition((prevScrollPosition) => prevScrollPosition + scrollVelocity * 0.1);
+    };
+
+    const animationFrame = requestAnimationFrame(updateAnimation);
+
+    return () => cancelAnimationFrame(animationFrame);
+  }, [scrollVelocity]);
+
 
   return (
     <div className={styles.Content} id="home">
-        <div className={styles.HeroImg}>
-            {/* <div className={styles.Glow}></div>
-            <div className={styles.Glow}></div> */}
-            <motion.img
-            initial={{ opacity: 0, y:50 }}
-             animate={{
-              y: [0, 10, 0],
-              opacity:[1, 1, 1]
-            }}
-            transition={{
-              delay:2,
-              duration: 2,
-              ease: "easeInOut",
-              times: [0,0.5,1],
-              repeat: Infinity,
-              repeatDelay: 0
-            }}
-            src={Hero} alt="Arke" />
-        </div>
-        <motion.h1
-        initial={{ opacity: 0, y:50 }}
-        animate={{ opacity: 1 ,y:0}}
+      <p className={styles.Quote}>"After the victory, Zeus removed her wings before throwing her into Tartaros"<p className={styles.Source}>-Ptolemy Hephaestion, New History</p></p>
+      <div className={styles.HeroImg}>
+
+        <motion.img
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: [0, 0.75]
+          }}
+          style={{ transform: `translateY(-${scrollPosition * 0.25}px)` }}
+          transition={{
+            delay: 1,
+            duration: 2,
+            ease: "easeInOut",
+          }}
+          src={Arke} alt="Arke" />
+        {/* <img
+          style={{ transform: `translateY(-${scrollPosition * 0.25}px)` }}
+          src={Arke} alt="Arke" /> */}
+      </div>
+      <motion.h1
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{
           type: "spring",
           stiffness: 160,
           damping: 20,
-          delay:1
+          delay: 1
         }}
-        >Arkē <span>Chat</span></motion.h1>
-        <motion.h2
-                initial={{ opacity: 0, y:50 }}
-                animate={{ opacity: 1 ,y:0}}
-                transition={{
-                  type: "spring",
-                  stiffness: 160,
-                  damping: 20,
-                  delay:1.25
-                }}
-        >Chat private. Chat free.</motion.h2>
-        <motion.p
-                initial={{ opacity: 0, y:50 }}
-                animate={{ opacity: 1 ,y:0}}
-                transition={{
-                  type: "spring",
-                  stiffness: 160,
-                  damping: 20,
-                  delay:1.5
-                }}
-        >Create your own disposable chatroom, invite other people to chat with you and not worry about anyone else snooping into your conversation. Nothing is saved.</motion.p>
-        <motion.div
-                initial={{ opacity: 0, y:50 }}
-                animate={{ opacity: 1 ,y:0}}
-                transition={{
-                  type: "spring",
-                  stiffness: 160,
-                  damping: 20,
-                  delay:1.75
-                }}
+      >Arkē <span>Chat</span></motion.h1>
+      <motion.h2
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 160,
+          damping: 20,
+          delay: 1.25
+        }}
+      >Chat private. Chat free.</motion.h2>
+      <motion.p
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 160,
+          damping: 20,
+          delay: 1.5
+        }}
+      >Create your own disposable chatroom, invite other people to chat with you and not worry about anyone else snooping into your conversation. Nothing is saved.</motion.p>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 160,
+          damping: 20,
+          delay: 1.75
+        }}
         className={styles.BtnGroup}>
-            <motion.a
-            href="https://app.arkechat.live"
-            whileTap={{ scale: 0.99 }}
-            className={styles.MainBtn} ref={MainButton}>
-            <div className={styles.BtnBorder}></div>
-                <div className={styles.BtnContent}>
-                <span>Get started</span> <IconChevronRight stroke={2} />
-                </div>
-            </motion.a>
-            <a href="#highlights" className={styles.SubBtn}> <button>Learn more</button></a>
-        </motion.div>
+        <motion.a
+          href="https://app.arkechat.live"
+          whileTap={{ scale: 0.99 }}
+          className={styles.MainBtn} ref={MainButton}>
+          <div className={styles.BtnBorder}></div>
+          <div className={styles.BtnContent}>
+            <span>Get started</span> <IconChevronRight stroke={2} />
+          </div>
+        </motion.a>
+        <a href="#highlights" className={styles.SubBtn}> <button>Learn more</button></a>
+      </motion.div>
     </div>
   )
 }
--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -8,22 +8,25 @@ import ArkeApp from "../assets/arkeapp.png"
 
 const Showcase = () => {
   return (
-    <div className={styles.Showcase} id="showcase">
+    <>
+      <div className={styles.Showcase} id="showcase">
         <div className={styles.TitleSequence}>
-            <img src={Blaze} alt="Blazing Fast" />
-            <div className={styles.Divider}></div>
-            <div className={styles.Titles}>
-                <h1>Sleek new ui.</h1>
-                <h1>Blazingly fast paced ux.</h1>
-            </div>
-            <div className={styles.Glow}></div>
+          <img src={Blaze} alt="Blazing Fast" />
+          <div className={styles.Divider}></div>
+          <div className={styles.Titles}>
+            <h1>Sleek new ui.</h1>
+            <h1>Blazingly intuitive ux.</h1>
+          </div>
+          <div className={styles.Glow}></div>
         </div>
         <p>Experience satisfying smooth animations, quick and intuitive actions, and tons of different features manufactured with you in mind. Don't ever let your tools slow you down! </p>
         <div className={styles.ImgFrame}>
           <img className={styles.ArkeWeb} src={ArkeWeb} alt="Arke Web UI" />
           <img className={styles.ArkeApp} src={ArkeApp} alt="Arke Web UI" />
         </div>
-    </div>
+
+      </div>
+    </>
   )
 }
 
